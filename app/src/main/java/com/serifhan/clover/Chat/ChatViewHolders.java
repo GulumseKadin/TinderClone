bfd7959a705853892c72eabd35a8be9ffbbeb3f5
Serifhan <serifhaniskl@gmail.com>
2019-08-01 07:57:01 +0300

    TinderClone TASARIM DISINDA

---
+++ app/src/main/java/com/serifhan/clover/Chat/ChatViewHolders.java
@@ -0,0 +1,24 @@
+package com.serifhan.clover.Chat;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.serifhan.clover.R;
+
+public class ChatViewHolders extends RecyclerView.ViewHolder implements View.OnClickListener{
+    public TextView mMessage;
+    public LinearLayout mContainer;
+    public ChatViewHolders(View itemView) {
+        super(itemView);
+        itemView.setOnClickListener(this);
+
+        mMessage = itemView.findViewById(R.id.message);
+        mContainer = itemView.findViewById(R.id.container);
+    }
+
+    @Override
+    public void onClick(View view) {
+    }
+}
---
+++ app/src/main/java/com/serifhan/clover/Matches/MatchesViewHolders.java
@@ -0,0 +1,37 @@
+package com.serifhan.clover.Matches;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.serifhan.clover.Chat.ChatActivity;
+import com.serifhan.clover.R;
+
+// Created by Serifhan Isıklı 18.07.2019;
+
+
+public class MatchesViewHolders extends RecyclerView.ViewHolder implements View.OnClickListener{
+    public TextView mMatchId, mMatchName;
+    public ImageView mMatchImage;
+    public MatchesViewHolders(View itemView) {
+        super(itemView);
+        itemView.setOnClickListener(this);
+
+        mMatchId = (TextView) itemView.findViewById(R.id.Matchid);
+        mMatchName = (TextView) itemView.findViewById(R.id.MatchName);
+
+        mMatchImage = (ImageView) itemView.findViewById(R.id.MatchImage);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(view.getContext(), ChatActivity.class);
+        Bundle b = new Bundle();
+        b.putString("matchId", mMatchId.getText().toString());
+        intent.putExtras(b);
+        view.getContext().startActivity(intent);
+    }
+}
